d92818b2deab6554530bee4a5427fbb6bb5a1109
omar <omarhrn@gmail.com>
2025-03-13 14:10:02 -0300

    instalamos shadcnui

--- src/components/header/ActionButtons.tsx
+++ src/components/header/ActionButtons.tsx
@@ -1,7 +1,26 @@
+import { Button } from "@/components/ui/button";
+
 export function DownloadButtons() {
   return (
     <div className="flex items-center justify-end gap-2">
-      
+      <Button
+        variant="outline"
+        className="text-black dark:text-white cursor-pointer"
+      >
+        Save Favicon
+      </Button>
+      <Button
+        variant="outline"
+        className="text-black dark:text-white cursor-pointer"
+      >
+        Download SVG
+      </Button>
+      <Button
+        variant="outline"
+        className="text-black dark:text-white cursor-pointer"
+      >
+        Download PNG
+      </Button>
     </div>
-  )
+  );
 }
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,9 +1,15 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { Header } from "../components/header/Header";
 
 export default function Layout() {
   return (
     <div className="flex flex-col w-full text-white h-screen">
       <Header />
+      <main className="grid grid-cols-[auto,1fr,auto] overflow-hidden w-full justify-center">
+        <SidebarProvider className="gap-2">
+
+        </SidebarProvider>
+      </main>
     </div>
   )
 }
